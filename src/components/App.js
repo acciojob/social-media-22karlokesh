9326e9ffb9f128392d2f1fb35cd942e7f50384d9
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-07-06 17:34:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,10 +13,9 @@
 // export default App
 
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function Posts() {
-  // Example post data
   const [posts, setPosts] = useState([
     { id: 1, author: "Alice", content: "Hello World!" },
     { id: 2, author: "Bob", content: "React is cool." },
@@ -32,13 +31,11 @@ function Posts() {
           </button>
         </div>
       ))}
-      {/* Add form or button to add posts if required */}
     </div>
   );
 }
 
 function Users() {
-  // Exactly 3 users as test expects
   const users = [
     { id: 1, name: "User One" },
     { id: 2, name: "User Two" },
@@ -60,34 +57,31 @@ function Notifications() {
   return (
     <div className="notifications">
       <button className="button">Mark All Read</button>
-      {/* You can add notification items here */}
     </div>
   );
 }
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <h1>GenZ</h1>
-        <nav>
-          <Link to="/posts" style={{ marginRight: "15px" }}>
-            Posts
-          </Link>
-          <Link to="/users" style={{ marginRight: "15px" }}>
-            Users
-          </Link>
-          <Link to="/notifications">Notifications</Link>
-        </nav>
+    <div>
+      <h1>GenZ</h1>
+      <nav>
+        <Link to="/posts" style={{ marginRight: "15px" }}>
+          Posts
+        </Link>
+        <Link to="/users" style={{ marginRight: "15px" }}>
+          Users
+        </Link>
+        <Link to="/notifications">Notifications</Link>
+      </nav>
 
-        <Routes>
-          <Route path="/" element={<Posts />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/notifications" element={<Notifications />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/notifications" element={<Notifications />} />
+      </Routes>
+    </div>
   );
 }
 
